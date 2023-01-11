43cc72ed017b1f01cf88e8e4866cf1f1cda6cf6d
sayat-bey <sayat.bey@ya.ru>
2023-01-11 22:05:56 +0600

    v2

--- main.py
+++ main.py
@@ -4,7 +4,6 @@ from sys import argv
 from datetime import datetime
 from pathlib import Path
 from netmiko import ConnectHandler
-from netmiko.ssh_exception import NetMikoTimeoutException, SSHException
 from jinja2 import Environment, FileSystemLoader
 from openpyxl import load_workbook
 
@@ -35,9 +34,9 @@ conf_dic = {
     "helpers": None,    # ["172.20.14.33", "172.20.24.170"]
     "vlans": None,      # [1010 1011 1012 .. 1015]
     "port": None,       # BS port
-    "ip": None,         # list of bs ip
-    "mask": None,
-    "ios_type": None,
+    "ip": None,         # [list of bs ip]
+    "mask": None,       # mask of bs
+    "ios_type": None,   # cisco_ios, cisco_xr, cisco_xe
     }
 
 configuration_log = []
@@ -67,10 +66,8 @@ def get_user_pw():
 
 
 def get_info():
-
     device = input("Enter device (ip or hostname): ")
     region = device.split("-")[0]
-    
     if "." in region:
         region_final = region.split(".")[1]
     else:
@@ -79,10 +76,9 @@ def get_info():
     return region_final, device
 
 
-def set_region(conf_dic, probable_region):
-
+def set_region(conf_dic, probable_region, helper):
     while True:
-        region = input(f"Enter region [{probable_region}]: ") or probable_region
+        region = input(f"Enter region for IP RELAY [{probable_region}]: ") or probable_region
         if helper.get(region):
             break
         else:
@@ -94,22 +90,26 @@ def set_region(conf_dic, probable_region):
 
 
 def set_ios(conf_dic, device):
-    
-    if "csg" in device:
-        ios_type = input("Enter ios (ios,xr,xe) [ios]: ") or "ios"
-    elif "pagg" in device:  
-        ios_type = input("Enter ios (ios,xr,xe) [xr]: ") or "xr"
+    while True:
+        if "csg" in device:
+            ios_type = input("Enter ios type (ios,xr,xe) [ios]: ") or "ios"
+        elif "pagg" in device:  
+            ios_type = input("Enter ios type (ios,xr,xe) [xr]: ") or "xr"
         
-    if ios_type == "ios":
-        conf_dic["ios_type"] = "cisco_ios"
-    elif ios_type == "xr":
-        conf_dic["ios_type"] = "cisco_xr"
-    elif ios_type == "xe":
-        conf_dic["ios_type"] = "cisco_xe"
+        if ios_type == "ios":
+            conf_dic["ios_type"] = "cisco_ios"
+            break
+        elif ios_type == "xr":
+            conf_dic["ios_type"] = "cisco_xr"
+            break
+        elif ios_type == "xe":
+            conf_dic["ios_type"] = "cisco_xe"
+            break
+        else:
+            print(f"#ERROR# Wrong ios type: {ios_type}. It must be one of ios,xr,xe")
 
 
 def connect(usr, psw, conf_dic, device):
-
     ssh_conn = ConnectHandler(device_type=conf_dic["ios_type"], ip=device, username=usr, password=psw)
     show_inf_desc = ssh_conn.send_command("show interfaces description")
 
@@ -117,79 +117,73 @@ def connect(usr, psw, conf_dic, device):
 
 
 def define_port_vlan(show_inf_desc, conf_dic):
+    print("########################################\n"
+         f"{show_inf_desc}\n"
+          "########################################")
     
+    vl = []     # 101 102 103
+    probable_last_vlan = "1010"
+
     if conf_dic["ios_type"] == "cisco_ios":
-        vl = []     # 101 102 103
-        probable_last_vlan = "1010"
-        
-        print("########################################\n"
-             f"{show_inf_desc}\n"
-              "########################################")
-        
         for line in show_inf_desc.splitlines():
             if "Vl10" in line:
                 vl.append(line.split()[0][2:5])     #Vl1010 -> 101
-        
-        if len(vl) > 1:
-            last_vlan = vl[-1]
-            probable_last_vlan = str(int(last_vlan)+1) + "0"
-            
-        vlan = input(f"Enter first vlan (1010,1020,..,10x0) [{probable_last_vlan}]: ") or probable_last_vlan
-        vlans = [str(int(vlan)+i) for i in range(6)]
-        conf_dic["vlans"] = vlans
-         
+
+    elif conf_dic["ios_type"] == "cisco_xr":  
+        for line in show_inf_desc.splitlines():
+            if ".10" in line:
+                vl.append(line.split()[0][-4:-1])     #interface.1010 -> 101
+
+    if len(vl) > 1:
+        last_vlan = vl[-1]
+        probable_last_vlan = str(int(last_vlan)+1) + "0"
+
+    vlan = input(f"Enter first vlan (1010,1020,..,10x0) [{probable_last_vlan}]: ") or probable_last_vlan
+    vlans = [str(int(vlan)+i) for i in range(6)]
+    conf_dic["vlans"] = vlans
     conf_dic["port"] = input("Enter BS interface: ")
     
 
 def load_excel(conf_dic):
     cwd = os.getcwd()
     files = []
+    ip_list = []
+    x = 1
+    y = 1
+
     for i in os.listdir(cwd):
         if os.path.isfile(i) and "xlsx" in i:
             files.append(i)
 
     print(f"xlsx files in current directory: [{', '.join(files)}]")	
-    
     probable_file = files[0]
-    
     excel_file = input(f"Enter BS excel file [{probable_file}]: ") or probable_file
-    ip_list = []
-
     wb = load_workbook(excel_file)
     first_sheet = wb.sheetnames[0]
     sheet = wb[first_sheet]
-
-    x = 1
-    y = 1
 
     while True:
         v = sheet.cell(row=x, column=y).value
         if "." in str(v) and len(v.split(".")) == 4:
             last_octet1 = v.split(".")[3]
-            
             v2 = sheet.cell(row=x, column=y+1).value
             v3 = sheet.cell(row=x, column=y+2).value
-            
             if "." in str(v2) and "." in str(v3):
                 last_octet2 = v2.split(".")[3]
                 if int(last_octet1) + 1 == int(last_octet2):
                     ip = sheet.cell(row=x, column=y).value
                     mask = sheet.cell(row=x, column=y+2).value
                     ip_list.append(ip)
-            
         y += 1
-        if y == 60:     # max limit
+        if y == 60:             # max limit
             y = 1
             x += 1
-
-        if len(ip_list) == 6:     # max limit
-            break
-            
-        if x == 30:     # max limit
+        if len(ip_list) == 6:   # max limit
+            break 
+        if x == 30:             # max limit
             print("########################################"
                  f"# ERROR check IP: {ip_list}")
             break
-    
     if len(ip_list) != 6:
         print("########################################"
              f"# ERROR check IP: {ip_list}")
@@ -199,41 +193,50 @@ def load_excel(conf_dic):
 
 
 def read_template(conf_dic):
-    
     template_output = ""
-    
+    env = Environment(loader=FileSystemLoader('./'))
+
     if conf_dic["ios_type"] == "cisco_ios":
-        env = Environment(loader=FileSystemLoader('./'))
-        template = env.get_template("csg-template.txt")
-        template_output = template.render(config = conf_dic)
-        
+        template = env.get_template("ios-template.txt")        
+    elif conf_dic["ios_type"] == "cisco_xr":
+        template = env.get_template("xr-template.txt")
+
+    template_output = template.render(config = conf_dic)
+
     return template_output.splitlines()
  
 
 def configure(ssh_conn, commands, configuration_log, cfg):
-
     if cfg:
         if len(commands) > 0:
             configuration_log.append(ssh_conn.send_config_set(commands))
+
+            if conf_dic["ios_type"] == "cisco_ios":
+                try:
+                    configuration_log.append(ssh_conn.save_config())
+                except Exception as err_msg:
+                    configuration_log.append(f"COMMIT is OK after msg:{err_msg}")
+                    configuration_log.append(ssh_conn.send_command("\n", expect_string=r"#"))
             
-            try:
-                configuration_log.append(ssh_conn.save_config())
-            except Exception as err_msg:
-                configuration_log.append(f"COMMIT is OK after msg:{err_msg}")
-                configuration_log.append(ssh_conn.send_command("\n", expect_string=r"#"))
-            
+            elif conf_dic["ios_type"] == "cisco_xr":
+                configuration_log.append(ssh_conn.send_command("show configuration"))
+                configuration_log.append(ssh_conn.commit())
+                ssh_conn.exit_config_mode()
+
         else:
-            print("########################################"
+            print("########################################\n"
                   "cfg is not needed")
     else:
         if len(commands) > 0:
-            print("########################################")
+            print("########################################\n"
+                  "candidate configuration:\n"
+                  "########################################")
+
             for line in commands:
                 print(line)
 
 
 def check_commit(configuration_log, commands, cfg):
-
     if cfg:
         for i in configuration_log:
             if "%" in i:
@@ -247,7 +250,6 @@ def check_commit(configuration_log, commands, cfg):
                         "ERROR. Not all config is loaded. Check cfg log")
 
 def write_logs(cfg):
-
     start_time = datetime.now()
     current_date = start_time.strftime("%Y.%m.%d")
     current_time = start_time.strftime("%H.%M")
@@ -272,15 +274,12 @@ def write_logs(cfg):
 # ------------------------------              ----------------------------------------#
 #######################################################################################
 
-print(
-    "\n"
-    "---------------------------------------------------------------------------"
-)
+print("\n---------------------------------------------------------------------------")
 
 cfg = get_arguments(argv)
 username, password = get_user_pw()
 region, device = get_info()
-set_region(conf_dic, region)
+set_region(conf_dic, region, helper)
 set_ios(conf_dic, device)
 
 try:
@@ -303,4 +302,3 @@ if ssh_conn:
     print("########################################\n"
           "success\n"
           "########################################")
-    
